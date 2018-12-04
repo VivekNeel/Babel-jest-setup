c36896d8c3257ea9bcede6ab22d03704ea613768
Vivek_Neel <vivek2neel@gmail.com>
2018-12-04 10:53:02 +0530

    initial commit

---
+++ Hello.js
@@ -0,0 +1,13 @@
+import React, { PureComponent } from "react";
+import { SafeAreaView, ScrollView } from "react-navigation";
+import { View } from "react-native";
+
+export default class Hello extends PureComponent {
+  render() {
+    return (
+      <SafeAreaView>
+        <ScrollView />
+      </SafeAreaView>
+    );
+  }
+}
