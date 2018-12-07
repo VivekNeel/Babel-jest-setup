a9f1d76e7f216d85ab8283f338754b76ea4d3e31
Vivek_Neel <vivek2neel@gmail.com>
2018-12-07 14:54:14 +0530

    Fixed directeventtypes undefined

--- Hello.test.js
+++ Hello.test.js
@@ -2,9 +2,11 @@ import React from "react";
 import Link from "./Hello";
 import renderer from "react-test-renderer";
 
+jest.mock('react-native-gesture-handler', () => {});
+
+
 it("renders correctly", () => {
-  const tree = renderer
-    .create(<Link page="http://www.facebook.com">Facebook</Link>)
-    .toJSON();
-  expect(tree).toMatchSnapshot();
-});
+      const tree = renderer
+        .create( < Link page = "http://www.facebook.com" > Facebook < /Link>)
+          .toJSON(); expect(tree).toMatchSnapshot();
+        });
